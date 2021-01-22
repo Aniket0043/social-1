9219c1a6d7fc6e4d0d9f9bc06489df5fefd02dd2
Aniket0043 <aniket9350@gmail.com>
2021-01-22 12:39:20 +0530

    second commit

---
+++ controllers/home_controller.js
@@ -0,0 +1,5 @@
+const router = require("../routes")
+
+module.exports.home=function(req, res){
+    return res.end("<h1>the aniket</h1>")
+}
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const port=8000;
+const app = express();
+
+//use express router 
+app.use("/",require("./routes/index"))
+
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`error in serving file: ${err}`)
+    }
+    console.log(`server is running:${port}`)
+})
