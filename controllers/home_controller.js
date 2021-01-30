5b6ff84ff5015b174e6e7c81dc00e387360dd286
Aniket0043 <aniket9350@gmail.com>
2021-01-31 00:12:26 +0530

    add users

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,9 @@
 const router = require("../routes")
-
-module.exports.home=function(req, res){
-    return res.render("home",{
-        title:"home"
-    })
-}
+const Post=require("../models/post")
+module.exports.home=function(req, res){ 
+    Post.find({}).populate("user").exec(function(err,posts){
+        return res.render("home",{
+            title:"home",
+            posts:posts
+        });
+    })}
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const homeController = require("../controllers/home_controller")
 
 router.get('/',homeController.home)
 router.use('/user',require("./user"))
+router.use("/posts",require("./post"))
 router.use("/message",require("./message"))
 console.log("router loaded")
 
